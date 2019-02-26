f878c3aad8843420007816bca5d88560e292a80b
Dorian <fouillen.dorian@orange.fr>
2019-02-26 16:41:34 +0100

    Ceci est un commit initial
    Oui

---
+++ Progeaiiit/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Progeaiiit.Controllers
+{
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        // GET: Identity
+        public ActionResult Identity()
+        {
+            return Content("We are using Identity");
+        }
+        public ActionResult NonIdentiy()
+        {
+            return Content("We are not using Identity");
+        }
+    }
+}
---
+++ Progeaiiit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Progeaiiit.MvcApplication" Language="C#" %>
