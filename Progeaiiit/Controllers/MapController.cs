21a07ec7e268bfd303f318853b47d3b6f3a6decc
Kevin <miistral85@yahoo.fr>
2019-03-04 10:30:50 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Progeaiiit/Controllers/MapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BO;
+
+namespace Progeaiiit.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            Race race = new Race();
+            return View();
+        }
+    }
+}
